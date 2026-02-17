6ce460c87eb19423cef7e5e2f6ca231f14c8c245
veerakotipalli13 <veerakotipalli13@gmail.com>
2026-02-17 10:46:25 -0700

    react with parcel

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const header = React.createElement("h1", {}, "Welcome to React Learning");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
